aeb99c81b187f7039f75a057537a10a3eddcec98
Charvi <charvigarg012@gmail.com>
2025-08-27 23:52:10 +0530

    [Charvi] added a new file

--- Core-Java/Level 1/AveragePCM.java
+++ Core-Java/Level 1/AveragePCM.java
@@ -1,14 +1,11 @@
 public class AveragePCM {
     public static void main(String[] args) {
-        // Marks out of 100
         int maths = 94;
         int physics = 95;
         int chemistry = 96;
 
-        // Calculate average
         double average = (maths + physics + chemistry) / 3.0;
 
-        // Output result
         System.out.println("Sam’s average mark in PCM is " + average + "%");
     }
 }
