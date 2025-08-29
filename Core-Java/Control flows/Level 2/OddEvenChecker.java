a90c5e1c1218d83d18414297597764ff5e0ccb54
Charvi <charvigarg012@gmail.com>
2025-08-29 10:30:33 +0530

    [Charvi] added a new file

---
+++ Core-Java/Control flows/Level 2/OddEvenChecker.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class OddEvenChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a natural number: ");
+        int number = scanner.nextInt();
+
+        if (number < 1) {
+            System.out.println("Please enter a natural number (greater than 0).");
+        } else {
+            System.out.println("\nOdd and Even numbers from 1 to " + number + ":");
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is an Even number");
+                } else {
+                    System.out.println(i + " is an Odd number");
+                }
+            }
+        }
+
+        scanner.close();
+    }
+}
