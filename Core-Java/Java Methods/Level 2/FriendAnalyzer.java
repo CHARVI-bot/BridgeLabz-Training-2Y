144f5dabff222412f4450756919f67178a63d550
Charvi <charvigarg012@gmail.com>
2025-09-05 12:53:41 +0530

    [Charvi] added a new file

--- Core-Java/Java Methods/Level 2/FriendAnalyzer.java
+++ Core-Java/Java Methods/Level 2/FriendAnalyzer.java
@@ -22,27 +22,26 @@ public class FriendAnalyzer {
         return maxIndex;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] names = {"Amar", "Akbar", "Anthony"};
-        int[] ages = new int[3];
-        double[] heights = new double[3];
+public static void main(String[] args) {
+    String[] names = {"Amar", "Akbar", "Anthony"};
+    int[] ages = new int[3];
+    double[] heights = new double[3];
 
-        System.out.println("Enter age and height for Amar, Akbar, and Anthony:");
+    System.out.println("Enter age and height for Amar, Akbar, and Anthony:");
 
+    try (Scanner scanner = new Scanner(System.in)) {
         for (int i = 0; i < names.length; i++) {
             System.out.print(names[i] + "'s age: ");
             ages[i] = scanner.nextInt();
             System.out.print(names[i] + "'s height (in cm): ");
             heights[i] = scanner.nextDouble();
         }
-
-        int youngestIndex = findYoungest(ages);
-        int tallestIndex = findTallest(heights);
-
-        System.out.println("\nYoungest friend: " + names[youngestIndex] + " (Age: " + ages[youngestIndex] + ")");
-        System.out.println("Tallest friend: " + names[tallestIndex] + " (Height: " + heights[tallestIndex] + " cm)");
-
-        scanner.close();
     }
+
+    int youngestIndex = findYoungest(ages);
+    int tallestIndex = findTallest(heights);
+
+    System.out.println("\nYoungest friend: " + names[youngestIndex] + " (Age: " + ages[youngestIndex] + ")");
+    System.out.println("Tallest friend: " + names[tallestIndex] + " (Height: " + heights[tallestIndex] + " cm)");
+}
 }
