d93be4b35befdaeb58428559c7c13b3da8643494
Charvi <charvigarg012@gmail.com>
2025-09-05 10:28:18 +0530

    [Charvi] added a new file

---
+++ Core-Java/Java Methods/Level 1/TrigonometryCalculator.java
@@ -0,0 +1,23 @@
+public class TrigonometryCalculator {
+
+    public double[] calculateTrigonometricFunctions(double angle) {
+        double radians = Math.toRadians(angle);
+
+        double sine = Math.sin(radians);
+        double cosine = Math.cos(radians);
+        double tangent = Math.tan(radians);
+
+        return new double[] {sine, cosine, tangent};
+    }
+
+    public static void main(String[] args) {
+        TrigonometryCalculator calc = new TrigonometryCalculator();
+        double angle = 45.0;
+        double[] results = calc.calculateTrigonometricFunctions(angle);
+
+        System.out.printf("Angle: %.2f degrees%n", angle);
+        System.out.printf("Sine: %.4f%n", results[0]);
+        System.out.printf("Cosine: %.4f%n", results[1]);
+        System.out.printf("Tangent: %.4f%n", results[2]);
+    }
+}
