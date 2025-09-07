a15aaaf1991e5ad6e3155269d47c74f8ea828d4a
Charvi <charvigarg012@gmail.com>
2025-09-07 22:40:35 +0530

    [Charvi] added a new file

---
+++ Core-Java/Java String/Level 2/CharacterClassifier.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class CharacterClassifier {
+
+    public static int getLength(String str) {
+        int count = 0;
+        try {
+            while (true) {
+                for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i); // assuming you're looping through a string
+
+                    if (Character.isDigit(ch)) {
+                        System.out.println(ch + " is a digit");
+                    } else if (Character.isLetter(ch)) {
+                        System.out.println(ch + " is a letter");
+                    } else {
+                        System.out.println(ch + " is a special character");
+                    }
+                }
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+        }
+        return count;
+    }
+
+    public static String classifyChar(char ch) {
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = (char)(ch + 32);
+        }
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return "Vowel";
+            } else {
+                return "Consonant";
+            }
+        } else {
+            return "Not a Letter";
+        }
+    }
+
+    public static String[][] analyzeCharacters(String str) {
+        int len = getLength(str);
+        String[][] result = new String[len][2];
+
+        for (int i = 0; i < len; i++) {
+            char ch = str.charAt(i);
+            result[i][0] = String.valueOf(ch);
+            result[i][1] = classifyChar(ch);
+        }
+
+        return result;
+    }
+
+    public static void displayTable(String[][] table) {
+        System.out.println("\nCharacter\tType");
+        System.out.println("-------------------------");
+        for (String[] row : table) {
+            System.out.printf("%-10s\t%s\n", row[0], row[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a string: ");
+            String input = sc.nextLine();
+
+            String[][] analysis = analyzeCharacters(input);
+            displayTable(analysis);
+        }
+    }
+}
