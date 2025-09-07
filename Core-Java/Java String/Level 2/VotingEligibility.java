3e21488927ffb8f2a4f6957da41671edf11261d1
Charvi <charvigarg012@gmail.com>
2025-09-07 22:52:39 +0530

    [Charvi] added a new file

---
+++ Core-Java/Java String/Level 2/VotingEligibility.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class VotingEligibility {
+
+    public static int[] generateAges(int n) {
+        Random rand = new Random();
+        int[] ages = new int[n];
+        for (int i = 0; i < n; i++) {
+            ages[i] = rand.nextInt(90) + 10; 
+        }
+        return ages;
+    }
+
+    public static String[][] checkEligibility(int[] ages) {
+        String[][] result = new String[ages.length][2];
+        for (int i = 0; i < ages.length; i++) {
+            int age = ages[i];
+            result[i][0] = String.valueOf(age);
+            if (age < 0) {
+                result[i][1] = "false"; 
+            } else if (age >= 18) {
+                result[i][1] = "true";
+            } else {
+                result[i][1] = "false"; 
+            }
+        }
+        return result;
+    }
+
+    public static void displayTable(String[][] data) {
+        System.out.printf("%-10s%-15s%n", "Age", "Can Vote");
+        System.out.println("-------------------------");
+        for (String[] row : data) {
+            System.out.printf("%-10s%-15s%n", row[0], row[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = scanner.nextInt();
+
+        int[] ages = generateAges(n);
+
+        String[][] eligibility = checkEligibility(ages);
+
+        displayTable(eligibility);
+    }
+}
