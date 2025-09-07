7bf6d7470398c282beaac0c8d1b3cc8d148ad892
Charvi <charvigarg012@gmail.com>
2025-09-07 20:24:07 +0530

    [Charvi] added a new file

--- Core-Java/Java String/Level 1/NullPointerDemo.java
+++ Core-Java/Java String/Level 1/NullPointerDemo.java
@@ -2,13 +2,13 @@ public class NullPointerDemo {
 
     public static void generateException() {
         String text = null;
-        System.out.println("Length of text: " + text.length());
+        System.out.println("Length: " + (text != null ? text.length() : 0));
     }
 
     public static void handleException() {
         String text = null;
         try {
-            System.out.println("Length of text: " + text.length());
+            System.out.println("Length: " + (text != null ? text.length() : 0));
         } catch (NullPointerException e) {
             System.out.println("Caught a NullPointerException!");
             System.out.println("Error: " + e.getMessage());
