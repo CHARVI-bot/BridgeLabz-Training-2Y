0b4e2cc66252ae903bfbda77883c8455be06e5b6
Charvi <charvigarg012@gmail.com>
2025-09-16 23:39:29 +0530

    [Charvi] added a new file

---
+++ Core-Java/Java String/Level 3/FirstUniqueCharFinder.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class FirstUniqueCharFinder {
+
+    public static char findFirstNonRepeatingChar(String text) {
+        int[] freq = new int[256]; 
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            freq[ch]++;
+        }
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (freq[ch] == 1) {
+                return ch;
+            }
+        }
+
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        char result = findFirstNonRepeatingChar(input);
+
+        if (result != '\0') {
+            System.out.println("First non-repeating character: " + result);
+        } else {
+            System.out.println("No non-repeating character found.");
+        }
+    }
+    }
+}
